4b5b81d85e28813b883df901951ebddc005e78cd
Sunshine <steixeira1337@gmail.com>
2022-06-08 14:40:13 -0300

    feat: 'create pages (profile, home, new)'

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,18 +2,24 @@ import React, { useContext } from 'react';
 import { View, Text, Button } from 'react-native';
 
 import { AuthContext } from '../../Contexts/auth';
+import Header from '../../Components/Header'
+
+import { Background, Container, Nome, Saldo, Title  } from './styles'
 
 export default function Home() {
 
-  const {user, signOut} = useContext(AuthContext);
+  const {user} = useContext(AuthContext);
+
  return (
-   <View>
-       <Text>{user && user.email}</Text>
-       <Text>{user && user.nome}</Text>
-       <Button
-        title="Sair da Conta"
-        onPress={ () => signOut() }
-       />
-   </View>
+      <Background>
+        <Header/>
+        <Container>
+          <Nome>Samuel</Nome>
+          <Saldo>R$ 5.000,00</Saldo>
+        </Container>
+
+        <Title>Ultimas movimentações</Title>
+
+      </Background>
   );
 }
---
+++ src/Pages/Profile/index.js
@@ -0,0 +1,33 @@
+import React, {useContext} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import { View, Text } from 'react-native';
+import Header from '../../Components/Header'
+
+import { AuthContext } from '../../Contexts/auth';
+
+import { Container, Nome, NewLink, NewText, Logout, LogoutText } from './styles'
+
+export default function Profile() {
+  
+  const navigation = useNavigation();
+
+  const {user, signOut} = useContext(AuthContext);
+
+ return (
+   <Container>
+        <Header/>
+       <Nome>
+            {user && user.nome}
+       </Nome> 
+
+       <NewLink onPress={() => navigation.navigate('Registrar')}>
+            <NewText>Registrar gastos</NewText>
+       </NewLink>
+
+       <Logout onPress={ () => signOut() }>
+         <LogoutText>Sair</LogoutText>
+       </Logout>
+
+   </Container>
+  );
+}
