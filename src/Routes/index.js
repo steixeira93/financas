185cd040354ec9df986ee0aa4b26ecd25cc1b8db
Sunshine <steixeira1337@gmail.com>
2022-06-08 11:56:32 -0300

    feat: 'loading context'

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react';
+import { View, ActivityIndicator } from 'react-native';
 import { AuthContext } from '../Contexts/auth'
 
 import AuthRoutes from './auth.routes'
@@ -6,7 +7,15 @@ import AppRoutes from './app.routes'
 
 function Routes() {
 
-    const {signed} = useContext(AuthContext);
+    const {signed, loading} = useContext(AuthContext);
+
+    if(loading){
+        return(
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <ActivityIndicator size="large" color="#131313" />
+            </View>
+        )
+    }
 
     return(
         signed ? <AppRoutes/> : <AuthRoutes/>
