c3154892271d1f187bea1973f083ddadc6d52cbd
Sunshine <steixeira1337@gmail.com>
2022-06-08 11:12:50 -0300

    feat: "signUp function"

--- src/Contexts/auth.js
+++ src/Contexts/auth.js
@@ -1,16 +1,35 @@
 import React, { createContext, useState } from 'react';
+import firebase from '../services/firebaseConnection';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
 
-    const [user, setUser] = useState({
-        nome: 'Samuel',
-        uid: '928374982734'
-    });
+    const [user, setUser] = useState(null);
+
+    
+    // Cadastrar usuário
+    async function signUp(email, password, nome){
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(async (value) => {
+            let uid = value.user.uid;
+            await firebase.database().ref('users').child(uid).set({
+                saldo: 0,
+                nome: nome
+            })
+            .then(() => {
+                let data = {
+                    uid: uid,
+                    nome: nome,
+                    email: value.user.email,
+                };
+                setUser(data);
+            })
+        })
+    }
 
     return(
-        <AuthContext.Provider value={ {user} }>
+        <AuthContext.Provider value={{ signed: !!user, user, signUp }}>
             {children}
         </AuthContext.Provider>
     );
