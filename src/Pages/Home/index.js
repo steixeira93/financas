8ef01be4753a96d8df9b13f6904765cddeab7f2d
Sunshine <steixeira1337@gmail.com>
2022-06-08 11:57:29 -0300

    feat: 'some changes'

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import { AuthContext } from '../../Contexts/auth';
 
 export default function Home() {
+
+  const {user, signOut} = useContext(AuthContext);
  return (
    <View>
-       <Text>Home</Text>
+       <Text>{user && user.email}</Text>
+       <Text>{user && user.nome}</Text>
+       <Button
+        title="Sair da Conta"
+        onPress={ () => signOut() }
+       />
    </View>
   );
 }
--- src/Pages/SignIn/index.js
+++ src/Pages/SignIn/index.js
@@ -12,10 +12,10 @@ export default function SignIn() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { user } = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext);
 
   function handleLogin() {
-
+    signIn(email, password);
   }
 
  return (
