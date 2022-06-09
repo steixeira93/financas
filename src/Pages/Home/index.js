cb0a802ea914ec01f80c68fe68f48d0d1ee3aabc
Sunshine <steixeira1337@gmail.com>
2022-06-09 14:58:15 -0300

    feat: "some changes"

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, Text, Button } from 'react-native';
 
 import { AuthContext } from '../../Contexts/auth';
 import Header from '../../Components/Header'
+import HistoricoList from '../../Components/HistoricoList'
 
-import { Background, Container, Nome, Saldo, Title  } from './styles'
+import { Background, Container, Nome, Saldo, Title, List  } from './styles'
 
 export default function Home() {
+
+  const [historico, setHistorico] = useState([
+    {key: '1', tipo: 'receita', valor: 1200},
+    {key: '2', tipo: 'despesa', valor: 300},
+    {key: '3', tipo: 'receita', valor: 40},
+    {key: '4', tipo: 'receita', valor: 250},
+    {key: '5', tipo: 'despesa', valor: 89.72},
+    {key: '6', tipo: 'despesa', valor: 35},
+    {key: '7', tipo: 'despesa', valor: 89.72},
+    {key: '8', tipo: 'despesa', valor: 400},
+  ]);
 
   const {user} = useContext(AuthContext);
 
@@ -19,6 +31,13 @@ export default function Home() {
         </Container>
 
         <Title>Ultimas movimentações</Title>
+
+        <List
+          showsVerticalScrollIndicator={false}
+          data={historico}
+          keyExtractor={item => item.key}
+          renderItem={ ( {item} ) => ( <HistoricoList data={item} /> )}
+        />
 
       </Background>
   );
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -29,4 +29,15 @@ export const Title = styled.Text`
     margin-bottom: 10px;
 `; 
 
+export const List = styled.FlatList.attrs({
+    marginHorizontal: 15
+})`
+    padding-top: 15px;
+    background-color: #fff;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    margin-left: 8px;
+    margin-right: 8px;
+`;
+
 
